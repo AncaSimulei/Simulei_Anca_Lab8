413b6599e37e637d27a31ddfffb9d6bb984470ff
Anca Simulei <anca.simuu@gmail.com>
2022-01-14 22:47:56 +0200

    Laboratorul 8

---
+++ Data/Simulei_Anca_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Simulei_Anca_Lab8.Models;
+
+namespace Simulei_Anca_Lab8.Data
+{
+    public class Simulei_Anca_Lab8Context : DbContext
+    {
+        public Simulei_Anca_Lab8Context (DbContextOptions<Simulei_Anca_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Simulei_Anca_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Simulei_Anca_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Simulei_Anca_Lab8.Models.Category> Category { get; set; }
+    }
+}
